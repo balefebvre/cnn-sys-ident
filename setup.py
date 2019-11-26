7c6be4bf7bad1768e8ec0429a3ff190b50d402a9
balefebvre <baptiste.lefebvre@ens.fr>
2019-11-26 13:48:53 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+packages = [
+    'cnn_sys_ident',
+]
+install_requires = [
+]
+setup_requires = [
+]
+test_requires = [
+]
+entry_points = {
+    'console_scripts': [
+    ],
+    'gui_scripts': [
+    ]
+}
+
+setup(
+    name='cnn_sys_ident',
+    version='0.0.0',
+    packages=packages,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    test_requires=test_requires,
+    entry_points=entry_points,
+)
